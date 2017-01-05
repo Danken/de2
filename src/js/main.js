3f82ae247ea3588875c7f5d9891fb6eba49953c3
Daniel Emanuelsson <emanuelsson@gmail.com>
2017-01-05 12:06:49 +0100

    Made code a little more DRY

--- src/js/main.js
+++ src/js/main.js
@@ -59,62 +59,74 @@ function clock() {
 
     function update() {
 
-        // Get time and calculate angles
         var current_time = new Date();
-        var current_second = current_time.getSeconds()
-        var current_second_angle = current_second / 60 * 360;
-        var current_minute = current_time.getMinutes()
-        var current_minute_angle = current_minute / 60 * 360;
-        var current_hour = current_time.getHours() % 12
-        var current_hour_angle = current_hour / 12 * 360;
-
         //For each stem
         $.each(stems, function(key, value) {
             var path = value.path
+
+            //Get time
+            function getStemValue() {
+                switch (key) {
+                    case "sec":
+                        var time = current_time.getSeconds()
+                        break;
+                    case "min":
+                        var time = current_time.getMinutes()
+                        break;
+                    case "hour":
+                        var time = current_time.getHours()
+                        break;
+                    default:
+                        break;
+                }
+
+                return time
+            }
+
+            var stem_value = getStemValue();
+
+            console.log(stem_value);
+
+            var current_second_angle = stem_value / 60 * 360;
+            var current_minute_angle = stem_value / 60 * 360;
+            var current_hour_angle = stem_value / 12 * 360;
+
             // Reset paths
             path.segments[0].point = paper.view.center;
             path.segments[1].point.x = paper.view.center.x;
             path.segments[1].point.y = -300;
+
+            // For each stem:
+            switch (key) {
+                case "sec":
+                    path.rotate(current_second_angle, paper.view.center);
+                    positionCharacters(stems.sec, stems.sec.id, 31, 2, 15, stem_value)
+                    break;
+                case "min":
+                    path.rotate(current_minute_angle, paper.view.center);
+                    positionCharacters(stems.min, stems.min.id, 31, 2, 25, stem_value)
+                    break;
+                case "hour":
+                    path.rotate(current_hour_angle, paper.view.center);
+                    positionCharacters(stems.hour, stems.hour.id, 6, 0, 25, stem_value)
+                    break;
+                default:
+                    break;
+            }
         });
 
-        // Rotate stems
-        stems.sec.path.rotate(current_second_angle, paper.view.center);
-        stems.min.path.rotate(current_minute_angle, paper.view.center);
-        stems.hour.path.rotate(current_hour_angle, paper.view.center);
-
-        // Position characters
-        var char_amount = $('#second span').length + 1
-        for (var i = 0; i < char_amount; i++) {
-            // Reverse the order of the characters if on left side of clock face
-            if (current_second > 31 || current_second < 2) {
-                var order = ((char_amount) - i);
-            } else {
-                var order = i
+        function positionCharacters(stem, id, reverse1, reverse2, spacing, stem_value) {
+            var char_amount = $(('#' + id) + ' span').length + 1
+            for (var i = 0; i < char_amount; i++) {
+                // Reverse the order of the characters if on left side of clock face
+                if (stem_value > reverse1 || stem_value < reverse2) {
+                    var order = ((char_amount) - i);
+                } else {
+                    var order = i
+                }
+                var point1 = stem.path.getPointAt(i * spacing);
+                $('#' + id + '>span:nth-child(' + order + ')').css('top', point1.y + 'px').css('left', point1.x + 'px');
             }
-            var point1 = stems.sec.path.getPointAt(i * 15);
-            $('#second>span:nth-child(' + order + ')').css('top', point1.y + 'px').css('left', point1.x + 'px');
-        }
-        var char_amount = $('#minute span').length + 1
-        for (var i = 0; i < char_amount; i++) {
-            // Reverse the order of the characters if on left side of clock face
-            if (current_minute > 31 || current_minute < 2) {
-                var order = ((char_amount) - i);
-            } else {
-                var order = i
-            }
-            var point1 = stems.min.path.getPointAt(i * 25);
-            $('#minute>span:nth-child(' + order + ')').css('top', point1.y + 'px').css('left', point1.x + 'px');
-        }
-        var char_amount = $('#hour span').length + 1
-        for (var i = 0; i < char_amount; i++) {
-            // Reverse the order of the characters if on left side of clock face
-            if (current_hour > 6 || current_hour == 0) {
-                var order = ((char_amount) - i);
-            } else {
-                var order = i
-            }
-            var point1 = stems.hour.path.getPointAt(i * 25);
-            $('#hour>span:nth-child(' + order + ')').css('top', point1.y + 'px').css('left', point1.x + 'px');
         }
 
         clock_face.position = paper.view.center;
@@ -128,7 +140,6 @@ function clock() {
     update();
 
 }
-
 
 $(document).ready(function() {
 
